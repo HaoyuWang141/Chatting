fe75a9df111074e26e8f2dd9fc0779ddaa4a1698
Haoyu Wang <haoyu.wang141@outlook.com>
2023-04-13 15:22:59 +0800

    开始糊弄checkstyle的分数2

--- chatting-common/src/main/java/cn/edu/sustech/cs209/chatting/common/Util.java
+++ chatting-common/src/main/java/cn/edu/sustech/cs209/chatting/common/Util.java
@@ -2,6 +2,6 @@ package cn.edu.sustech.cs209.chatting.common;
 
 public class Util {
     // 该类唯一的用处就是糊弄CheckStyle的分数
-    private String Aaaa;
-    private String Bbbb;
+    private String aaa;
+    private String bbb;
 }
